454402da11494e6402fba0ce53b5405be1039c21
Daniel Andrade <danielandrader23@gmail.com>
2024-02-20 11:56:27 -0500

    creacion de nueva rama y agrego mis datos.

---
+++ src/main/java/co/edu/ucentral/daniel/Danielandrade.java
@@ -0,0 +1,5 @@
+package co.edu.ucentral.daniel;
+
+public class Danielandrade {
+    String datos = "mi nombre es daniel andrade estudio ing en sistemas, y mi correo es dandrader2@ucentral.edu.co";
+}
